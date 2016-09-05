06580526644e9729bb037b930e42bc61f3be811b
letocollection <leonard@letocollection.com>
2016-09-04 19:14:09 -0700

    yes

--- spotify.js
+++ spotify.js
@@ -1,10 +1,14 @@
 var spotify = require('spotify');
+var request = require("request");
  
 spotify.search({ type: 'track', query: 'dancing in the moonlight' }, function(err, data) {
     if ( err ) {
         console.log('Error occurred: ' + err);
         return;
     }
- 
-    lookup: function({ type: 'artist OR album OR track', id: 'Spotify ID Hash' }, hollaback)
+
+    console.log(data);
+
+    var queryUrl = 'https://api.spotify.com/v1/search?query=' + songName + '&offset=0&limit=20&type=track'
+
 });
